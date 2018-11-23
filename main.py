9ce0853664d8e50b15c65110a57f84695e127614
Subhendu Ranjan Mishra <subhendu.r.mishra@gmail.com>
2018-11-23 15:15:43 +0530

    basic version

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import make_response, Flask, json
+import requests
+from flask_cors import CORS, cross_origin
+from gevent.pywsgi import WSGIServer, LoggingLogAdapter
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.route("/api/hello")
+def say_hello():
+    return make_response("Hello")
+
+@app.route('/api/qa', methods=['POST'])
+def answer_qa():
+    return "I am thinking about it"
+
+def start_http_server():
+    '''
+        Start the API server over HTTP
+    '''
+    # logger.info("Starting HTTP Server")
+    http_server = WSGIServer(('0.0.0.0', 8080),
+                             app,)
+    http_server.serve_forever()
+
+if __name__ == "__main__":
+    start_http_server()
